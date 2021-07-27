31a439dc97196f33f3809a04b3a492801401097a
Evelin Aasna <evelin.aasna@fu-berlin.de>
2021-07-27 10:26:29 +0200

    [MISC] remove bovine from MG-1

--- MG-1/scripts/tax_to_bins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BINS=152
-
-for ((i=0;i<=BINS;i++)); do
-	awk '$6==i {print $1}' bovine_out/bininfo.tsv > binIDs
-	grep -A 1 -f binIDs ../bovine_gut/nogCOGdomN95.seq | sed '/--/d' > "bovine_out/${i}.fasta"
-	rm binIDs
-done
