b9f206cabc7e761fb35341b72d89d4ac03202a2c
Evelin Aasna <evelin.aasna@fu-berlin.de>
2021-11-02 13:49:55 +0100

    Fix splitting read file into equal parts

--- raptor_data_simulation/scripts/custom_read_files.sh
+++ raptor_data_simulation/scripts/custom_read_files.sh
@@ -7,10 +7,20 @@ cd $p
 rm bin*
 rm all_10.fastq
 
-nr=${5}
-echo "dividing reads into ${nr} read files"
+num_files=${5}
+echo "Dividing reads into ${num_files} read files"
 
-split -nr/"${nr}" all.fastq reads_
+
+a=($(wc all.fastq))
+total_lines=${a[0]}
+
+# https://stackoverflow.com/questions/7764755/how-to-split-a-file-into-equal-parts-without-breaking-individual-lines
+((lines_per_file = (total_lines + num_files - 1) / num_files))
+
+split --lines=${lines_per_file} all.fastq reads_
+
+echo "Total number of reads = $((total_lines / 4))"
+echo "Reads per file = $((lines_per_file / 4))"
 
 k=0
 for file in reads_*; do
